4adfbb87806e6a4421f1f3a784a851b97f07222f
SashaPlavi <sasha.bingulac@gmail.com>
2020-07-17 22:15:58 +0200

    functions

---
+++ index.ts
@@ -0,0 +1,24 @@
+const myName: string = "Sasa";
+
+const ten: number = 10;
+
+// const add = (a: number, b: number): number => {
+//   return a + b;
+// };
+
+// const joinstring = (a: string, b: string): string => {
+//   return a + b;
+// };
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+// const joinstring = function (a: string, b: string): string {
+//   return a + b;
+// };
+
+const post: { title: string; day: number; published: boolean } = {
+  title: "my post",
+  day: 5,
+  published: true,
+};
